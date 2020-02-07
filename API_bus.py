bd67e277cefd73833b9db2e37f110b7b9e5cf6f7
kimcnama <kimcnama@tcd.ie>
2020-02-07 21:50:14 +0000

    try except for APIs

--- API_bus.py
+++ API_bus.py
@@ -13,23 +13,27 @@ class API_bus:
         self.empty_msg = "No RTPI Info"
 
     def make_api_call(self):
-        response = requests.get(self.url)
-        if response.status_code == 200:
-            json_response = json.loads(response.content.decode('utf-8'))
-            if json_response:
-                # time in seconds
-                self.bus_info = []
-                self.time_stamp_last_call = time.time()
-                results = json_response['results']
-                for bus in results:
-                    if 'Due' in bus['duetime']:
-                        self.bus_info.append("{} in {}".format(bus['route'], bus['duetime']))
-                    else:
-                        self.bus_info.append("{} in {}mins".format(bus['route'], bus['duetime']))
-                if not self.bus_info:
-                    self.bus_info.append(self.empty_msg)
-        else:
+        try:
+            response = requests.get(self.url)
+            if response.status_code == 200:
+                json_response = json.loads(response.content.decode('utf-8'))
+                if json_response:
+                    # time in seconds
+                    self.bus_info = []
+                    self.time_stamp_last_call = time.time()
+                    results = json_response['results']
+                    for bus in results:
+                        if 'Due' in bus['duetime']:
+                            self.bus_info.append("{} in {}".format(bus['route'], bus['duetime']))
+                        else:
+                            self.bus_info.append("{} in {}mins".format(bus['route'], bus['duetime']))
+                    if not self.bus_info:
+                        self.bus_info = [self.empty_msg]
+            else:
+                print("Error calling Bus API stop: {}".format(self.stop_num))
+        except:
             print("Error calling Bus API stop: {}".format(self.stop_num))
+            self.bus_info = [self.empty_msg]
 
 if __name__ == '__main__':
     bus = API_bus("184")
